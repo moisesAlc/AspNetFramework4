1dfb072f1f73fc0cdac515bdfa0201791f43c89a
Moisés Madeira <moisesprepara@gmail.com>
2022-05-30 19:30:13 -0300

    added support page

---
+++ WebFormsApplication/Support.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Support.aspx.cs" Inherits="WebFormsApplication.Support" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="lblIssueDescription" runat="server">
+        <div>
+            <h1 id="literalAknowledgeMessage">Support</h1>
+        </div>
+        <asp:Literal ID="ltlAcknowledgeMessage" runat="server"></asp:Literal>
+        <br />
+        <br />
+        First Name:<br />
+        <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="valFirstName" runat="server" ControlToValidate="txtFirstName" ErrorMessage="RequiredFieldValidator" ForeColor="Red">First Name is required.</asp:RequiredFieldValidator>
+        <br />
+        <asp:Label ID="lblLastName" runat="server" Text="Last Name:"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="E-mail:"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="valEmail" runat="server" ControlToValidate="txtEmail" ForeColor="Red">Email is required.</asp:RequiredFieldValidator>
+        <br />
+        Issue Description:<br />
+        <asp:TextBox ID="txtIssueDesc" runat="server" TextMode="MultiLine" Width="350px"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+    </form>
+</body>
+</html>
---
+++ WebFormsApplication/Support.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace WebFormsApplication
+{
+
+
+    public partial class Support
+    {
+
+        /// <summary>
+        /// Controle lblIssueDescription.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm lblIssueDescription;
+
+        /// <summary>
+        /// Controle ltlAcknowledgeMessage.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltlAcknowledgeMessage;
+
+        /// <summary>
+        /// Controle txtFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFirstName;
+
+        /// <summary>
+        /// Controle valFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator valFirstName;
+
+        /// <summary>
+        /// Controle lblLastName.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLastName;
+
+        /// <summary>
+        /// Controle txtLastName.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLastName;
+
+        /// <summary>
+        /// Controle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controle txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Controle valEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator valEmail;
+
+        /// <summary>
+        /// Controle txtIssueDesc.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtIssueDesc;
+
+        /// <summary>
+        /// Controle btnSubmit.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
+    }
+}
